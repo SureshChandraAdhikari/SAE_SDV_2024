d8d06d79b58f576d99a9e12bed6521953f767f9e
CharantejaPochepalli <charantejapochepalli@gmail.com>
2023-12-16 10:57:32 +0530

    https://github.com/CharantejaPochepalli/SAE_SDV_1.git

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BottomNavb from './Components/Bottomnav/Bottomnavbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <BottomNavb/>
   </React.StrictMode>
 );
 
